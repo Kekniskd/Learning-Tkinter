4b919e49d5a3605574e6e719258830f01b25da82
kedar <kekniskedar@gmail.com>
2024-04-20 12:28:12 +0530

    04/20/24 12:28:12 — main: Learned about window and widgets in Tkinter Signed-off-by: kedar <kekniskedar@gmail.com>

---
+++ kmToMiles.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+# from tkinter import ttk
+import ttkbootstrap as ttk
+
+
+def convert():
+    km_input = entry_int.get()
+    mile_output = km_input * 0.621371
+    output_string.set(str(round(mile_output, 2)))
+
+
+# window
+window = tk.Tk()
+window.title('Converter')
+window.geometry('400x150')
+
+# title
+title_label = ttk.Label(master=window, text='Convert kilometer to miles', font='calibri 18 bold')
+title_label.pack()
+
+input_frame = ttk.Frame(master=window)
+entry_int = tk.DoubleVar()
+entry = ttk.Entry(master=input_frame, textvariable=entry_int)
+button = ttk.Button(master=input_frame, text='Convert', command=convert)
+
+entry.pack(pady=5)
+button.pack()
+input_frame.pack(pady=10)
+
+output_string = tk.StringVar()
+output_label = ttk.Label(master=window,
+                         text='Output',
+                         font='calibri 14',
+                         textvariable=output_string)
+output_label.pack()
+
+window.mainloop()
